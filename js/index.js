0a58c1c9892b3697361aac2d11b1e196a55c9fff
JacobWellner83 <jcwellner83@gmail>
2019-04-13 11:57:50 -0500

    Event 2 Added

--- js/index.js
+++ js/index.js
@@ -1,7 +1,7 @@
 // Your code goes here
 
 //Event 1
-// Created which returns the altered Alt text as an allert when the intro image is moused over.
+// Created an event which returns the altered Alt text as an allert when the intro image is moused over.
 let desertBus = document.querySelector('.intro img');
 let desertBusAlt = desertBus.alt;
 
@@ -9,3 +9,18 @@ desertBus.addEventListener('mouseover', function(event) {
 window.alert(`${desertBusAlt}`)
 event.stopPropagation()
 });
+
+//Event 2
+// Created an event which causes images that are moused over to shift to grayscale and when the mouse leaves them, to disappear.
+let contentImages = document.querySelectorAll('img')
+contentImages.forEach(img => {
+    img.addEventListener('mouseover', function() {
+        this.style.filter = 'grayscale(100%)'
+        this.style.opacity = 1
+    })
+
+    img.addEventListener('mouseleave', function() {
+        this.style.filter = 'grayscale(0%)'
+        this.style.opacity = 0
+    })
+})

fe80117e595ede30b2b907f9f31a572181867bea
JacobWellner83 <jcwellner83@gmail>
2019-04-13 11:43:51 -0500

    Created First Event

--- js/index.js
+++ js/index.js
@@ -1,1 +1,11 @@
 // Your code goes here
+
+//Event 1
+// Created which returns the altered Alt text as an allert when the intro image is moused over.
+let desertBus = document.querySelector('.intro img');
+let desertBusAlt = desertBus.alt;
+
+desertBus.addEventListener('mouseover', function(event) {
+window.alert(`${desertBusAlt}`)
+event.stopPropagation()
+});

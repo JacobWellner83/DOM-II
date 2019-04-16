86accd652e2df43df1b8b78368621121cc9c3998
JacobWellner83 <jcwellner83@gmail>
2019-04-16 08:32:03 -0500

    Added Event 7

--- js/index.js
+++ js/index.js
@@ -23,7 +23,7 @@ contentImages.forEach(img => {
         this.style.filter = 'grayscale(0%)'
         this.style.opacity = 0
     })
-})
+});
 
 //Event 3- Scroll
 // Created an event which returns an alert when the user scrolls to the bottom of the page.
@@ -31,19 +31,19 @@ window.addEventListener('scroll', () => {
     if ((window.innerHeight + window.scrollY) >= document.body.offsetHeight) {
       alert(`That's all there is.`)
     }
-})
+});
 
 //Event 4- Load
-//Creates an event which produces an alert on initiation.
+//Creates an event which produces an alert on initiation and reload.
 window.addEventListener('load', function(event) {
     alert('Why did you reload me, Dave?');
-})   
+})  ; 
 
 //Event 5- Keydown
 //Creates an event in which if a key is pressed down an alert populates.
-window.addEventListener('keydown', function(event){
+window.addEventListener('keydown', function(event) {
     alert('You pressed P? Why? You just dont listen.');
-})
+});
 
 //Event 6- Dblclick
 //Creates an event that adds a Black border to the Sunny Fun Button when it is Double Clicked.
@@ -52,3 +52,11 @@ let button = document.querySelector(".btn");
 button.addEventListener("dblclick", function() {
     this.style.border = "thick solid black";
 });
+
+//Event 7- Resize
+//Created an Event that alters the font size of h4s when the window is resized.
+let resizeFont = document.querySelectorAll('.resizer');
+
+window.addEventListener('resize', function(event) {
+resizeFont.style.fontWeight = 40;
+})
